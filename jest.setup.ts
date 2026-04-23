58727ac67932d5778dea78dd41214b30f7b97769
Santi <yepes.s07@gmail.com>
2026-04-23 14:34:03 -0500

    Config test JEN

---
+++ jest.setup.ts
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+}));
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (/validateDOMNesting|not wrapped in act/.test(args[0])) return;
+  originalError.call(console, ...args);
+};
